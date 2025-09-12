33c7f2bbb7942a92a09e97276f7e2fcfcdadd462
Jefferson Teles <58746739+JeffersonTeles314@users.noreply.github.com>
2025-09-11 21:34:41 -0500

    Refactor file structure and update course cards
    Moved static assets to organized folders (img, style, script), updated references in index.html, and removed the old style.css. Standardized course card content and added a construction icon for incomplete courses using JavaScript. Updated styles to support new structure and improved maintainability.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Website com informações dos cursos do PROCiPy e suas respectivas estatítticas
+
+
+
+
+
+### Aos Desenvolvedores:
+
+Manteja o arquivo index.html com este nome
--- script/script.js
+++ script/script.js
@@ -14,3 +14,26 @@ const observer = new IntersectionObserver((entries) => {
 document.querySelectorAll('.fade-up').forEach(el => {
     observer.observe(el);
 });
+
+
+
+
+
+
+const emConstrucaoIcon = '<img src="img/R.svg" alt="Em Construção" class="em-construcao-icon"/>'
+
+
+function moverConteudo(Classe_div) {
+    // pega a div de origem (primeira que encontrar)
+    const origem = document.querySelectorAll(`.${Classe_div}`);
+
+    if (origem.length > 0) {
+      for (const div of origem) {
+        // transfere o conteúdo HTML
+        div.querySelector('.resumo-texto').innerHTML = emConstrucaoIcon;
+      }
+    }
+  }
+
+  // exemplo de uso
+  moverConteudo("em-construcao");
